5d11ce91f2e90a89af2b2c174250cbc1843e42af
eblis..e.a.zhao <eblis.e.a.zhao@gmail.com>
2013-12-12 23:32:00 +0800

    i Can run hahaha

--- HelloNN/src/main/java/com/zhaowei/HelloNN/HelloAndroidActivity.java
+++ HelloNN/src/main/java/com/zhaowei/HelloNN/HelloAndroidActivity.java
@@ -4,6 +4,10 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class HelloAndroidActivity extends Activity {
 
@@ -17,6 +21,14 @@ public class HelloAndroidActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button button = (Button) findViewById(R.id.button1);
+        button.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View arg0) {
+                TextView textView = (TextView) findViewById(R.id.textView1);
+                textView.setText("you click me");
+            }
+        });
     }
 
     @Override
